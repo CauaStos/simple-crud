b9ac15a7cce410a6b79910808c4ca8c01a56bdc4
Astro <caua.oli.santos@gmail.com>
2025-12-30 10:02:52 -0300

    feat(admin): add navigation cards

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,20 @@
+import NavCard from "@/components/shared/NavCard";
+
 export default function Page() {
-    return <div className="flex flex-1 justify-center items-center">WIP</div>;
+    return (
+        <div className="flex flex-1 justify-center items-center gap-10">
+            <NavCard
+                title="Usuários"
+                description="Gerencie todos os usuários"
+                content="Visualize, edite e gerencie contas de usuários."
+                href="/admin/usuarios"
+            />
+            <NavCard
+                title="Campanhas"
+                description="Gerencie as campanhas"
+                content="Crie, visualize e edite campanhas."
+                href="/admin/campanhas"
+            />
+        </div>
+    );
 }

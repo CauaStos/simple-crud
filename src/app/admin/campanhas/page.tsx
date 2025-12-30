5d022e1a1377ef69b2d21901e936b12d6c1f8a0d
Astro <caua.oli.santos@gmail.com>
2025-12-29 22:52:20 -0300

    feat(campaigns): implement campaigns page

--- src/app/admin/campanhas/page.tsx
+++ src/app/admin/campanhas/page.tsx
@@ -1,3 +1,39 @@
-export default function Page() {
-    return <div className="flex flex-1 justify-center items-center">WIP</div>;
+import prisma from "@/lib/prisma";
+import filteredPaginate, {
+    DEFAULT_SORT,
+} from "@/lib/pagination/filteredPaginate";
+import CampaignTable from "@/features/campaigns/components/table/CampaignTable";
+
+const PAGE_SIZE = 10;
+
+export default async function Page({
+    searchParams,
+}: {
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+    const params = await searchParams;
+
+    const {
+        items: campaigns,
+        pageCount,
+        rowCount,
+    } = await filteredPaginate(
+        prisma.campaign,
+        ["name", "description"],
+        params,
+        PAGE_SIZE,
+        DEFAULT_SORT,
+        "Campanha",
+    );
+
+    return (
+        <div className="flex flex-1 justify-center items-center">
+            <CampaignTable
+                campaigns={campaigns}
+                pageSize={PAGE_SIZE}
+                pageCount={pageCount}
+                rowCount={rowCount}
+            />
+        </div>
+    );
 }
